70218d413d675605b42d14741403226e4d2ed9f4
Simon Altman <simonzevaltman@gmail.com>
2026-01-16 01:13:26 -0500

    chore: Trigger deployment - merge conflicts resolved

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1876,7 +1876,6 @@ export default function AdminDashboard() {
           </div>
         )}
         
-<<<<<<< HEAD
         {analyticsSubTab === 'data-details' && (
           <div className="bg-white border border-gray-200 rounded-lg overflow-hidden">
             <div className="p-6 border-b border-gray-200">
